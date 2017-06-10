3dcf33d2356e6d1a48d4c7b64e1aaae8ed405784
fourspower <fourspower@users.noreply.github.com>
2017-06-10 15:59:27 +0200

    First ommit
    10.06.2017 marcello

---
+++ realtime statagiespiel/Assets/MoneyManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour {
+
+    public float kosten;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ realtime statagiespiel/Assets/triggert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggert : MonoBehaviour
+{
+    public List<Collider> triggers;
+
+    void OnTriggerEnter(Collider other)
+    {
+        triggers.Add(other);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        triggers.Remove(other);
+    }
+}
